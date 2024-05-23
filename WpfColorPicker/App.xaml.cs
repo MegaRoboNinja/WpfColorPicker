7b42a395917daa0783a6674eacdddb9e1cb73088
Miłosz W <131467705+MegaRoboNinja@users.noreply.github.com>
2024-05-23 18:31:31 +0200

    Add project files.

---
+++ WpfColorPicker/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfColorPicker
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
